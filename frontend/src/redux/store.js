3ae0f95d79198a620f035c6dbc870b59f9c21331
techvogue <vishwagautam57@gmail.com>
2025-07-27 05:04:25 +0530

    Initial project setup with CI/CD, Docker, and ignores

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import eventReducer from './slices/eventSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    events: eventReducer,
+    // add other slices here
+  }
+});
