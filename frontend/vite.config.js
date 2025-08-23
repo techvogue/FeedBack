406a3154e4f86ac99d653ad711dee98ea010322d
techvogue <vishwagautam57@gmail.com>
2025-08-23 18:39:47 +0530

    fixed some minor issue in vite..config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',  // 👈 Important for Vercel deployment
   server: {
     host: '0.0.0.0',
     port: 5173,
     watch: {
-      usePolling: true
-    }
+      usePolling: true,
+    },
   },
   preview: {
     host: '0.0.0.0',
-    port: 5173
-  }
+    port: 5173,
+  },
 })
